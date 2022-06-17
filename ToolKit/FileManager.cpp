6f2d853493839e5439a746a0b534211272a6a7d5
Osman Fatih Çakır <[cakirosmanfatih@gmail.com]>
2022-06-17 19:40:27 +0300

    Zip min resources

--- ToolKit/FileManager.cpp
+++ ToolKit/FileManager.cpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <unordered_map>
+#include <memory>
 
 #include "ToolKit.h"
 
@@ -11,8 +12,18 @@ namespace ToolKit
   void FileManager::PackResources(const String& path)
   {
     LoadAllScenes(path);
-    GetAllUsedResourcePaths();
-    CreatePackResources(path);
+    GetAllUsedResourcePaths(path);
+    //CreatePackResources(path);
+    
+    if (!CompressPack())
+    {
+      // Error
+      GetLogger()->WriteConsole(LogType::Error, "Error zipping.");
+    }
+    else
+    {
+      GetLogger()->WriteConsole(LogType::Warning, "Resources packed.");
+    }
   }
 
   void FileManager::LoadAllScenes(const String& path)
@@ -35,7 +46,7 @@ namespace ToolKit
     }
   }
 
-  void FileManager::GetAllUsedResourcePaths()
+  void FileManager::GetAllUsedResourcePaths(const String& path)
   {
     std::unordered_map<String, ResourcePtr> mp;
 
@@ -111,6 +122,13 @@ namespace ToolKit
 
       m_textureResourcePaths.insert(absolutePath);
     }
+
+    // Animations
+    static String animPath = ConcatPaths({ ResourcePath(), "Meshes", "anim" });
+    GetAnimationPaths(animPath);
+
+    // Scenes
+    GetScenePaths(path);
   }
 
   void FileManager::CreatePackResources(const String& path)
@@ -328,6 +346,133 @@ namespace ToolKit
         m_minSceneDirectoryPath.c_str()
       );
       return;
+    }
+  }
+
+  bool FileManager::CompressPack()
+  {
+    UniqueStringArray allPaths;
+    allPaths.insert(m_fontResourcePaths.begin(), m_fontResourcePaths.end());
+    allPaths.insert(m_materialResourcePaths.begin(), m_materialResourcePaths.end());
+    allPaths.insert(m_meshResourcePaths.begin(), m_meshResourcePaths.end());
+    allPaths.insert(m_shaderResourcePaths.begin(), m_shaderResourcePaths.end());
+    allPaths.insert(m_textureResourcePaths.begin(), m_textureResourcePaths.end());
+    allPaths.insert(m_animResourcePaths.begin(), m_animResourcePaths.end());
+    allPaths.insert(m_sceneResourcePaths.begin(), m_sceneResourcePaths.end());
+
+    String zipName = ConcatPaths({ ResourcePath(), "..", "MinResources.pak" });
+    zipFile zFile = zipOpen64(zipName.c_str(), 0);
+
+    if (zFile == NULL)
+    {
+      printf("Error opening zip.\n");
+      zipClose(zFile, NULL);
+      return false;
+    }
+
+    // Add files to zip
+    for (String path : allPaths)
+    {
+      if (!AddFileToZip(zFile, path.c_str()))
+      {
+        GetLogger()->WriteConsole(LogType::Error, "Error adding file to zip.");
+        return false;
+      }
+    }
+
+    zipClose(zFile, NULL);
+
+    return true;
+  }
+
+  bool FileManager::AddFileToZip(zipFile zfile, const char* filename)
+  {
+    FILE* f;
+    int ret;
+    size_t red;
+    size_t flen;
+
+    if (zfile == NULL || filename == NULL)
+    {
+      return false;
+    }
+
+    f = fopen(filename, "rb");
+    if (f == NULL)
+    {
+      return false;
+    }
+
+    fseek(f, 0, SEEK_END);
+    flen = ftell(f);
+    rewind(f);
+
+    String filenameStr = String(filename);
+    size_t index = filenameStr.find("Resources");
+    if (index != String::npos)
+    {
+      constexpr int length = sizeof("Resources");
+      filenameStr = filenameStr.substr(index + length);
+    }
+    else
+    {
+      GetLogger()->WriteConsole(LogType::Error, "Resource is not under resources path: %s", filename);
+      return false;
+    }
+
+    ret = zipOpenNewFileInZip64(zfile, filenameStr.c_str(), NULL, NULL, 0, NULL, 0, NULL,
+      Z_DEFLATED, Z_DEFAULT_COMPRESSION, (flen > 0xffffffff) ? 1 : 0);
+    if (ret != ZIP_OK)
+    {
+      fclose(f);
+      zipCloseFileInZip(zfile);
+      return false;
+    }
+
+    char* file_data = (char*)malloc((flen + 1) * sizeof(char));
+    red = fread(file_data, flen, 1, f);
+    ret = zipWriteInFileInZip(zfile, file_data, red * flen);
+    if (ret != ZIP_OK)
+    {
+      fclose(f);
+      zipCloseFileInZip(zfile);
+      return false;
+    }
+
+    free(file_data);
+    fclose(f);
+    zipCloseFileInZip(zfile);
+
+    return true;
+  }
+
+  void FileManager::GetAnimationPaths(const String& path)
+  {
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+    {
+      if (entry.is_directory())
+      {
+        // Go animations in directories
+        GetAnimationPaths(entry.path().string());
+        continue;
+      }
+
+      m_animResourcePaths.insert(entry.path().string());
+    }
+  }
+
+  void FileManager::GetScenePaths(const String& path)
+  {
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+    {
+      if (entry.is_directory())
+      {
+        // Go scenes in directories
+        GetScenePaths(entry.path().string());
+        continue;
+      }
+
+      m_sceneResourcePaths.insert(entry.path().string());
     }
   }
 
--- ToolKit/FileManager.h
+++ ToolKit/FileManager.h
@@ -2,6 +2,8 @@
 
 #include <filesystem>
 
+#include "zip.h"
+
 #include "Types.h"
 
 namespace ToolKit
@@ -13,9 +15,15 @@ namespace ToolKit
 
    private:
     void LoadAllScenes(const String& path);
-    void GetAllUsedResourcePaths();
+    void GetAllUsedResourcePaths(const String& path);
     void CreatePackResources(const String& path);
     void CreatePackDirectories();
+    
+    bool CompressPack();
+    bool AddFileToZip(zipFile zfile, const char* filename);
+    
+    void GetAnimationPaths(const String& path);
+    void GetScenePaths(const String& path);
 
     void CopyFontResourcesToPack();
     void CopyMaterialResourcesToPack();
@@ -31,6 +39,8 @@ namespace ToolKit
     UniqueStringArray m_meshResourcePaths;
     UniqueStringArray m_shaderResourcePaths;
     UniqueStringArray m_textureResourcePaths;
+    UniqueStringArray m_animResourcePaths;
+    UniqueStringArray m_sceneResourcePaths;
 
     Path m_minFontsDirectoryPath;
     Path m_minMaterialsDirectoryPath;
